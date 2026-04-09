0164eaa203b4b46cee98155be0a394bfab13a6b1
Bacem Karray <99637476+bacemkarray@users.noreply.github.com>
2026-04-09 17:57:53 -0400

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n+1);
+
+        for (int i=0; i<n+1; i++) {
+            dp[i] = dp[i>>1] + (i&1);
+        }
+        return dp;
+    }
+};
