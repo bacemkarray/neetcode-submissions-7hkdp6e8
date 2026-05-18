d7a4138af0548dfb05bd775fc12cca151ee0d160
Bacem Karray <99637476+bacemkarray@users.noreply.github.com>
2026-05-18 11:51:44 -0400

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    unordered_set<int> visited;
+    unordered_map<int,vector<int>> adj;
+
+    bool dfs(int crs, vector<vector<int>>& prerequisites) {
+        if (visited.find(crs) != visited.end()) return false;
+        visited.insert(crs);
+        for (int pre : adj[crs]) {
+            if (!(dfs(pre, prerequisites))) return false;
+        }
+        visited.erase(crs);
+        return true;  
+    }
+
+
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        for (const auto& pre : prerequisites) {
+            int c = pre[0];
+            int p = pre[1];
+            adj[c].push_back(p);
+        }
+
+        for (int i=0; i<numCourses; i++) {
+            if (!(dfs(i,prerequisites))) return false;
+        }
+        return true;
+    }
+};
