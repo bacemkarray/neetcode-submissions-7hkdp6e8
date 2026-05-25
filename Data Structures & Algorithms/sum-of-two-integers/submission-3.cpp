df856d43215bd4819a15579e60840ef8c38ec8a1
Bacem Karray <99637476+bacemkarray@users.noreply.github.com>
2026-05-25 00:15:38 -0400

    Add: sum-of-two-integers - submission-3

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int c = 0;
+        int res = 0;
+
+        for (int i=0; i<32; i++) {
+            int bit_a = (a >> i) & 1;
+            int bit_b = (b >> i) & 1;
+            int s = bit_a ^ bit_b ^ c;
+            c = (bit_a & bit_b) | ((bit_a | bit_b) & c);
+            res |= (s << i);
+        }
+        return res ;
+    }
+};
