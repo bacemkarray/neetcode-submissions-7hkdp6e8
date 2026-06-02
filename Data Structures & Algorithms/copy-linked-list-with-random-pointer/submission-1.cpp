a133575bad12858a9300d413263c3d4f784e8684
Bacem Karray <99637476+bacemkarray@users.noreply.github.com>
2026-06-02 08:27:14 -0400

    Add: copy-linked-list-with-random-pointer - submission-1

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-1.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node* curr = head;
+        unordered_map<Node*,Node*> table;
+
+        while (curr != nullptr) {
+            table[curr] = new Node(curr->val);
+            curr = curr->next;
+        }
+
+        curr = head;
+        while (curr != nullptr) {
+            table[curr]->next = table[curr->next];
+            table[curr]->random = table[curr->random];
+            curr = curr->next;
+        }
+        return table[head];
+    }
+};
